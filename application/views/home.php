edb4cde1a219aa69da050672e40f3cf6be275015
BRYAN <modybry@gmail.com>
2019-03-20 19:04:23 +0800

    myGithub

---
+++ application/views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/js/javascript">
+	<title>Log In</title>
+</head>
+<body>
+	<div class="text-center border border-light p-5">
+		<form>
+			<label>Name :</label><br>
+			<input type="text" name=""><br>
+			<label>Password</label><br>
+			<input type="Password" name=""><br>
+			<input type="submit" name="" class="btn btn-success">
+
+		</form>
+	</div>
+
+</body>
+</html>
